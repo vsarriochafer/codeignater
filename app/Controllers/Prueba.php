28656fb40abd25f56ae2021e0405189317b4ad16
Vicent Sarrió Chafer <VICENT_SARRIO@ajuntament.local>
2020-03-17 19:47:29 +0100

    Primer commit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,20 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		//return view('welcome_message');
+    echo "Esta es la función index";
+	}
+
+	//--------------------------------------------------------------------
+  public function test()
+  {
+    echo "Esta es la función test";
+  }
+  public function test2()
+  {
+    echo "Esta es la función test2";
+  }
+}
